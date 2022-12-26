44ab9164db542f9021dade7962c9d739aed239f5
Hay_Abadi <abados9891@gmail.com>
2022-12-26 23:20:32 +0200

    firstCommit

---
+++ C#/CampaignProject/CampaignProject.Entity/MainManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CampaignProject.Entity
+{
+    public class MainManager
+    {
+        private MainManager() { }
+
+        // Singleton variable
+        private static readonly MainManager instance = new MainManager();
+        public static MainManager Instance
+        {
+            get { return instance; }
+        }
+    }
+}
